c7f296c406ce6a23baecd7e639c545934d43cada
steckinho <112144017+steckinho@users.noreply.github.com>
2026-04-18 20:05:34 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,15 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  root: '.',
+  build: {
+    rollupOptions: {
+      input: './index.html'
+    }
+  },
+  resolve: {
+    alias: {
+      '/src': '/Src'
+    }
+  }
 })
